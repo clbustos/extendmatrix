f6272e20a1a1bebe0e374dfe5850e738d8b19edf
Cosmin Bonchis <cbonchis@info.uvt.ro>
2007-08-17 13:48:23 +0000

    no needed for the project

--- nodel.rb
+++
@@ -1,7 +0,0 @@
-class Tempfile
-	alias_method :old_initialize, :initialize
-  def initialize(basename, tmpdir=Dir::tmpdir, unlink_finalize = true)
-		old_initialize(basename, tmpdir)
-		ObjectSpace.undefine_finalizer(self) if not unlink_finalize
-	end
-end
